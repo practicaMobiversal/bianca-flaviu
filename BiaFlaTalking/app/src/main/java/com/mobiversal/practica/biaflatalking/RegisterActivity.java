62fa9fd19e219bfadd0d7d4b3122ed24d971cbd6
tflaviu <turcut.flv@gmail.com>
2017-07-10 13:15:17 +0300

    Edited app's colours

--- BiaFlaTalking/app/src/main/java/com/mobiversal/practica/biaflatalking/RegisterActivity.java
+++ BiaFlaTalking/app/src/main/java/com/mobiversal/practica/biaflatalking/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,11 +19,12 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText mDisplayName;
-    private EditText mEmail;
-    private EditText mPassword;
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
     private Button mCreateBtn;
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +32,27 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mAuth = FirebaseAuth.getInstance();
-        mDisplayName = (EditText) findViewById(R.id.reg_display_name);
-        mEmail = (EditText) findViewById(R.id.reg_email);
-        mPassword = (EditText) findViewById(R.id.reg_password);
+        mDisplayName = (TextInputLayout) findViewById(R.id.reg_display_name);
+        mEmail = (TextInputLayout) findViewById(R.id.reg_email);
+        mPassword = (TextInputLayout) findViewById(R.id.reg_password);
         mCreateBtn = (Button) findViewById(R.id.reg_create_btn);
 
-         mCreateBtn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 String display_name = mDisplayName.getText().toString();
-                 String email = mEmail.getText().toString();
-                 String password = mPassword.getText().toString();
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String display_name = mDisplayName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
 
-                 register_user (display_name, email, password);
-             }
-         });
+                register_user(display_name, email, password);
+            }
+        });
+        mToolbar = (Toolbar) findViewById(R.id.register_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Create account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 
     private void register_user(String display_name, String email, String password) {
@@ -52,14 +60,18 @@ public class RegisterActivity extends AppCompatActivity {
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+
                 if (task.isSuccessful()) {
-                    Intent maintIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                    startActivity(maintIntent);
+                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
                     finish();
                 } else {
                     Toast.makeText(RegisterActivity.this, "Error creating account", Toast.LENGTH_LONG).show();
                 }
             }
         });
+
     }
+
+
 }
