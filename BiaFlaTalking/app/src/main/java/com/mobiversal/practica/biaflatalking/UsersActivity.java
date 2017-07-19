c352039086e86dbbc96c25d5910e639a3415f244
BiaBulc <bianca.bulc@gmail.com>
2017-07-19 10:04:05 +0300

    The Users can be seen now

--- BiaFlaTalking/app/src/main/java/com/mobiversal/practica/biaflatalking/UsersActivity.java
+++ BiaFlaTalking/app/src/main/java/com/mobiversal/practica/biaflatalking/UsersActivity.java
@@ -2,8 +2,15 @@ package com.mobiversal.practica.biaflatalking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class UsersActivity extends AppCompatActivity {
@@ -12,6 +19,7 @@ public class UsersActivity extends AppCompatActivity {
 
     private Toolbar mToolBar;
     private RecyclerView mUsersList;
+    private DatabaseReference mUsersDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +27,61 @@ public class UsersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_users);
 
         mToolBar=(Toolbar) findViewById(R.id.users_appBar);
-        getSupportActionBar().setTitle("All Users");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setSupportActionBar(mToolBar);
 
-        mUsersList=(RecyclerView) findViewById(R.id.users_list);
+        getSupportActionBar().setTitle("All Users");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mUsersDatabase= FirebaseDatabase.getInstance().getReference().child("Users");
+
+        mUsersList=(RecyclerView) findViewById(R.id.users_list);
+        mUsersList.setHasFixedSize(true);
+        mUsersList.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+      FirebaseRecyclerAdapter<Users, UsersViewHolder>firebaseRecyclerAdapter =new FirebaseRecyclerAdapter<Users, UsersViewHolder>(
+
+              Users.class,
+              R.layout.users_single_layout,
+              UsersViewHolder.class,
+              mUsersDatabase
+
+
+
+      ) {
+          @Override
+          protected void populateViewHolder(UsersViewHolder usersviewHolder, Users users, int i) {
+               usersviewHolder.setDisplayName(users.getName());
+          }
+      };
+
+      mUsersList.setAdapter(firebaseRecyclerAdapter);
+
+
+    }
+
+    public static class UsersViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public UsersViewHolder(View itemView) {
+            super(itemView);
+
+            mView=itemView;
+        }
+       public void setDisplayName(String name){
+
+           TextView userNameView=(TextView) mView.findViewById(R.id.user_single_name);
+           userNameView.setText(name);
+       }
+       public void setUserStatus(String status){
+           TextView userStatusView=(TextView)mView.findViewById(R.id.user_single_status);
+           userStatusView.setText(status);
+       }
     }
 }
